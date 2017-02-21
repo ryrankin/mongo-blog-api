447934867b5e9d35142797135f76fab11ec21097
ryan Rankin <ryrankin@gmail.com>
2017-02-21 12:05:44 -0500

    final

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
+exports.DATABASE_URL = 'mongodb://ryrankin:Mn7u5g5a223@ds157459.mlab.com:57459/mongo-blog-api' ||
+						process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/mongo-blog-api';
+                      'mongodb://localhost/blogs';
 exports.PORT = process.env.PORT || 8080;
